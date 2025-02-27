54cbae2691cdb451668b8d11601ca2c908d3a249
Matteo Romano <matteoriserva0411@gmail.com>
2025-02-27 17:43:59 +0100

    asg

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('Barban').addEventListener('change', function() {
-    var selectedPage = this.value;
-    if (selectedPage) {
-        window.location.href = selectedPage;
-    }
-});
